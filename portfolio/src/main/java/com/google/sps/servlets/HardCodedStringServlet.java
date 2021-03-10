95d2f79c5125ca25d114dc0753e599883eb02aa0
Tino <tazvitya@grinnell.edu>
2021-03-10 21:35:47 +0000

    change ArrayList<String> messages to List<String> messages

--- portfolio/src/main/java/com/google/sps/servlets/HardCodedStringServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HardCodedStringServlet.java
@@ -15,7 +15,7 @@ public class HardCodedStringServlet extends HttpServlet {
     
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    ArrayList<String> messages = new ArrayList<String>();
+    List<String> messages = new ArrayList<String>();
     //add messages to arraylist messages
     messages.add("I'm a golden state warriors fan!");
     messages.add("My favorite Tv show is How to get away with murder");
