6d9193915ac13a3319637bd944d8e3f1a1a2a2dd
Tino <tazvitya@grinnell.edu>
2021-03-15 17:03:14 +0000

    Sanitize user input

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,6 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,29 +13,31 @@ import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
 import com.google.cloud.datastore.KeyFactory;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the values entered in the form.
-    String messageValue = request.getParameter("message-input");
-    String emailValue = request.getParameter("email");
-    
-    // Print the values so you can see it in the server logs.
-    System.out.println("Email submitted: " + emailValue);
-    System.out.println("Message submitted: " + messageValue);
-    
+    // Sanitize user input to remove Html tags and JavaScript
+    String messageValue = Jsoup.clean(request.getParameter("message-input"),Whitelist.none());
+    String emailValue = Jsoup.clean(request.getParameter("email"), Whitelist.none());
+  
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("ContactMe");
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Message");
     FullEntity messageEntity =
     Entity.newBuilder(keyFactory.newKey())
-        .set("Message", messageValue)
-        .set("Email", emailValue)
+        .set("message", messageValue)
+        .set("email", emailValue)
         .build();
-    datastore.put(messageEntity);
     
+    datastore.put(messageEntity);
+
     response.sendRedirect("/index.html");
   }
+
+  
 }
