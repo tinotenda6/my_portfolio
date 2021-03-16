62fe1a795d5912def8af7570c529ed55bd690afa
Tino <tazvitya@grinnell.edu>
2021-03-16 02:14:59 +0000

    fix: remove extra ;

--- portfolio/src/main/java/com/google/sps/data/Message.java
+++ portfolio/src/main/java/com/google/sps/data/Message.java
@@ -6,7 +6,6 @@ public final class Message {
   private final long id;
   private final String email;
   private final String message;
-  ;
 
   public Message(long id, String email, String message) {
     this.id = id;
